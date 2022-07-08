1646c1fd7da1522ce3b1b0ba533a90eae9f8aec0
猫猫 <overflowcat@gmail.com>
2022-07-08 21:00:35 +0800

    feat: ConstantTimeCompare

--- api/auth.go
+++ api/auth.go
@@ -8,13 +8,28 @@ import (
 	"os"
 )
 
+func ConstantTimeCompare(a, b string) bool {
+	a_len := len(a)
+	b_len := len(b)
+	if a_len != b_len {
+		return false
+	}
+	a_bytes := []byte(a)
+	b_bytes := []byte(b)
+	var result uint8
+	for i := 0; i < a_len; i++ {
+		result |= a_bytes[i] ^ b_bytes[i]
+	}
+	return result == 0
+}
+
 type AuthReq struct {
 	Password string `json:"password"`
 }
 
 func Auth(password string) bool {
 	passwd := os.Getenv("SIMPUBLISH_PASSWD")
-	return passwd == password
+	return ConstantTimeCompare(passwd, password)
 }
 
 func AuthHandler(w http.ResponseWriter, r *http.Request) {
